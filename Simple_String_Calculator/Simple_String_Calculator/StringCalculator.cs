fdf2ef54200bdb1a0d146c2c543082456120c9e0
Ghyad Adnan <ghayd.adnan2311@gmail.com>
2022-03-28 16:11:40 +0300

    Build a StringCalaculator class has two methods Add and ConvertToIntegers

---
+++ Simple_String_Calculator/Simple_String_Calculator/StringCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Simple_String_Calculator
+{
+    internal class StringCalculator
+    {
+        public int[] iNumbers { get; set; }
+        public int sum;
+
+        public int Add(string numbers)
+        {
+            sum = 0;
+            if (string.IsNullOrEmpty(numbers)) return sum;
+            iNumbers = ConvertToIntegers(numbers);
+            foreach (var i in iNumbers)
+            {
+                sum = sum + i;  
+            }
+            return sum;
+        }
+        public int[] ConvertToIntegers(string numbers)
+        {
+            string[] str_num = numbers.Split(',', '\n').ToArray();
+            int[] int_num = Array.ConvertAll(str_num, Int32.Parse);
+            return int_num;
+        }
+    }
+}
